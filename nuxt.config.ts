ef478d7565032e957766eff99c9ba5fe50bf1be8
Roger <roger8426@gmail.com>
2026-04-10 19:44:53 +0800

    fix: head icon遺失，改為相對路徑

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -13,7 +13,7 @@ export default defineNuxtConfig({
   app: {
     baseURL: process.env.NUXT_APP_BASE_URL ?? '/',
     head: {
-      link: [{ rel: 'icon', type: 'image/png', href: '/dice-20.png' }],
+      link: [{ rel: 'icon', type: 'image/png', href: './dice-20.png' }],
       title: 'Rolling Dice',
     },
   },
